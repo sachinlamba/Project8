fb41702ec08914197b42c295fbabd759328d219c
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-25 19:01:36 +0530

    Submission of Solution and clear button functions work here.Score is updating.

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -16,19 +16,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     
     @IBAction func submitTapped(sender: AnyObject) {
+        if let solutionPosition = solutions.indexOf(currentAnswer.text!) {
+            activatedButtons.removeAll()
+            
+            var splitClues = answersLabel.text!.componentsSeparatedByString("\n")
+            splitClues[solutionPosition] = currentAnswer.text!
+            answersLabel.text = splitClues.joinWithSeparator("\n")
+            
+            currentAnswer.text = ""
+            score += 1
         
+        
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done", message: "Are you ready for the next level?", preferredStyle: .Alert)
+                ac.addAction(UIAlertAction(title: "Lets's go", style: .Default, handler: levelUp))
+                presentViewController(ac, animated: true, completion: nil)
+            }
+        }
     }
     
     
     @IBAction func clearTapped(sender: AnyObject) {
+        currentAnswer.text = ""
         
+        for btn in activatedButtons {
+            btn.hidden = false
+        }
+        
+        activatedButtons.removeAll()
     }
     
     var letterButtons = [UIButton]()
     var activatedButtons = [UIButton]()
     var solutions = [String]()
     
-    var score = 0
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
     
     
@@ -99,6 +125,17 @@ class ViewController: UIViewController {
         currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
         activatedButtons.append(btn)
         btn.hidden = true
+    }
+    
+    func levelUp(action: UIAlertAction!) {
+        level += 1
+        solutions.removeAll(keepCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons {
+            btn.hidden = false
+        }
     }
     
 }
